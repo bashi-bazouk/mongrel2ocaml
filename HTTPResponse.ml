ba4b646a3511c43e886a599ffbcd436695e689ce
Cubit <cubit@BS-CLIENT.local>
2014-02-19 17:33:54 -0500

    Add in Content-Length if it's not there

--- HTTPResponse.ml
+++ HTTPResponse.ml
@@ -11,8 +11,14 @@ type http_response = {
 
 let mongrel2_response_of_http_response (uuid: string) (ids: int list)
     (r: http_response): mongrel2_response =
-  let headers = 
-    String.concat "\r\n" (map (fun (k,v) -> sprintf "%s:%s" k v) r.headers)  in
+  let headers =
+    let headers = 
+      if mem_assoc "Content-Length" r.headers then
+	r.headers
+      else
+	let content_length = string_of_int (String.length r.body) in
+	("Content-Length", content_length)::r.headers in
+    String.concat "\r\n" (map (fun (k,v) -> sprintf "%s:%s" k v) headers)  in
   let mongrel2_response_body =
     sprintf "HTTP/1.1 %i %s\r\n%s\r\n\r\n%s" r.code r.status headers r.body in
   { uuid = uuid;
